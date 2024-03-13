edd6617efe9e8bbae16c8cf36ba668d317d5c2a2
Kongjiyoung <ij0512@naver.com>
2024-03-13 21:16:57 +0900

    user resume 엔티티 수정

--- src/main/java/com/many/miniproject1/user/User.java
+++ src/main/java/com/many/miniproject1/user/User.java
@@ -3,9 +3,12 @@ package com.many.miniproject1.user;
 import jakarta.persistence.*;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
 
 import java.sql.Timestamp;
 
+@NoArgsConstructor
 @Table(name = "user_tb")
 @Data
 @Entity // 테이블 생성하기 위해 필요한 어노테이션
@@ -14,7 +17,7 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY) // auto_increment 전략
     private Integer id;
     private String role; // perso=-n, \]
-
+    @Column(unique = true)
     private String email;
     private String password;
     private String username;
@@ -24,5 +27,22 @@ public class User {
     private String companyNum;
     private String profile;
     private String birth;
+    @CreationTimestamp
     private Timestamp createdAt;
+
+    @Builder
+    public User(Integer id, String role, String email, String password, String username, String tel, String companyName, String address, String companyNum, String profile, String birth, Timestamp createdAt) {
+        this.id = id;
+        this.role = role;
+        this.email = email;
+        this.password = password;
+        this.username = username;
+        this.tel = tel;
+        this.companyName = companyName;
+        this.address = address;
+        this.companyNum = companyNum;
+        this.profile = profile;
+        this.birth = birth;
+        this.createdAt = createdAt;
+    }
 }
